b73de803b2dd3888101874219ff99ffab39d7914
Ruslan Gimranov <g1mran@bk.ru>
2022-11-08 13:31:57 +0300

    Step 2. Data transorm and created component with random character

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,42 +1,112 @@
+// импортируем компонент из реакта
+import { Component } from 'react';
+
+import MarvelService from '../../services/MarvelService';
+
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
+// import thor from '../../resources/img/thor.jpeg'; - не нужно, т.к. мы убрали ссылку на тора
 import mjolnir from '../../resources/img/mjolnir.png';
 
-const RandomChar = () => {
-    return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
+// используем класс реакта, т.к. функциональный метод пока не освоили
+class RandomChar extends Component {
+    // добавляем конструктор, чтобы вызвать написанный метод.
+    // ВАЖНО! При таком варианте браузер ругается, т.к. наш базовый стейт сформирован позже
+    // на данном этапе оставляем так, но такой вариант написания использовать не рекомендуется
+    constructor(props) {
+        super(props);
+        this.updateChar();
+    }
+
+    // задаем базовые состояния. Все будут нулевыми
+    state = {
+        // name: null,
+        // description: null,
+        // thumbnail/*картинка-превьюшка*/: null,
+        // homepage: null,
+        // wiki: null
+
+        // упрощаем запись, создавая ТОЛЬКО пустой объект char
+        char: {}
+    }
+
+    marvelService = new MarvelService(); // данный метод забираем из index.js и немного преобразовываем
+    
+    onCharLoaded = (char) => {
+        this.setState({char})
+    }
+    
+    // создаем метод получения персонажа (с помощью стрелочной функции)
+    updateChar = () => {
+        // пока устанавливаем конкретный id (хардкод, офк никакого рандома)
+        // const id = 1011005;
+
+        // создаем переменную id, которая теперь формируется РАНДОМНО!
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        // применяем к экземпляру marvelService метод получения персонажа
+        this.marvelService
+            /* Проверяем метод getAllCharacters, выводя объекты
+            в консоль. Результатом будет вывод объектов в нужном нам формате
+            НО - он выведется ДВАЖДЫ, всё из-за ввода метода в конструктор*/
+            // .getAllCharacters()
+            // .then(res => console.log(res))
+
+            .getCharacter(id)
+            // then используется, т.к. это промис
+            // .then(res => {
+                // перерисовываем базовые параметры используя получаемые данные
+                
+                // upd. выносим this.setState чтобы было красивее
+                // this.setState(res) // т.к. мы улучшили код, здесь используем просто res вместо кода ниже
+                    // name: res.data.results[0].name,
+                    // description: res.data.results[0].description,
+                    // thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+                    // homepage: res.data.results[0].urls[0].url,
+                    // wiki: res.data.results[0].urls[1].url
+            // })
+            .then(this.onCharLoaded) // итоговая версия с использованием char
+    }
+
+    render() {
+        // достаём переменные из объекта с помощью деструктуризации. НЕ ЗАБЫВАТЬ ПРО this.!
+        // const {name, description, thumbnail, homepage, wiki} = this.state;
+        
+        // адаптируем деструктуризацию под наличие char
+        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        return (
+            <div className="randomchar">
+                <div className="randomchar__block">
+                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                            {description}
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RandomChar;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,11 +1,11 @@
 
 // создаем класс на нативном JavaScript (поэтому не нужно импортировать компонент из реакта и на него завязывать наследование, это просто джаваскрипт)
 class MarvelService {
-    // для удобства части ссылки заворачиваем в переменные. Такой формат написания (с лодоша) переменных для понимания, что данные переменные нельзя изменять
+// для удобства части ссылки заворачиваем в переменные. Такой формат написания (с лодоша) переменных для понимания, что данные переменные нельзя изменять
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=a37a7031ad14e3d953e039b3e362f5c7';
 
-    // Используем метод getResource для получения данных с сервера в формате json
+// Используем метод getResource для получения данных с сервера в формате json
     getResource = async (url) => {
         let res = await fetch(url); // используем метод fetch
     
@@ -16,14 +16,38 @@ class MarvelService {
         return await res.json();
     }
 
-    // создаем метод получения информации о всех персонажах(лимит апишки 20)
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+// создаем метод получения информации о всех персонажах(лимит апишки 20)
+    getAllCharacters = async () => {
+        // return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        
+        /* применяем аналогичные изменения кода для этого метода*/
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    // создаем метод получения конкретного персонажа (нужен айди)
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+// создаем метод получения конкретного персонажа (нужен айди)
+/*т.к. getResource асинхронная функция, необходимо дождаться её ответа*/
+    getCharacter = async(id) => {
+        // return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        
+        // теперь мы не возвращаем персонажа, а сохраняем в промежуточный результат
+        const res = await/*парный оператор с async*/ this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+        /* т.к. у нас res.data.results[0] повторялось, можно его использовать как аргумент, тогда 
+        в методе можно использовать аргумент char, это значительно упростит код*/
+    }
+
+// создаем метод трансформирования объекта, полученного с сервера, в том формате, который нас интересует
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            // Домашнее задание - если описания нет, вывести текст, а если описание слишком больше, обрезать его и дополнить троеточием
+            description: char.description ? `${char.description.slice(0,210)}...` : 'There is no description for this character',
+            // конец ДЗ
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
